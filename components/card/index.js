4a7e3b0a768f7722210f6064df3e4ede91efed91
David Abram <david@abram.com>
2018-06-02 14:32:37 +0200

    changed card

--- components/card/index.js
+++ components/card/index.js
@@ -5,6 +5,17 @@ import styles from './index.css';
 export default class Card extends React.Component {
   render() {
     console.log(this.props);
-    return <div>Card</div>;
+    return (
+      <div className={styles.card}>
+        <img src="https://www.w3schools.com/howto/img_avatar.png"
+          alt="Avatar"
+          style={{ width: '100%' }}
+        />
+        <div className={styles.container}>
+          <h4><b>John Doe</b></h4>
+          <p>Architect & Engineer</p>
+        </div>
+      </div>
+    );
   }
 }
